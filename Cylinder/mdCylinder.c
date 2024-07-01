0a88960be023530dec17714a6a1a402a8463b33a
logrin <giovanni.delmonte90@gmail.com>
2024-07-01 20:45:38 +0200

    again

--- Cylinder/mdCylinder.c
+++ Cylinder/mdCylinder.c
@@ -1273,7 +1273,7 @@ void dumpsnapshot(particle* dumpevent)
     //Schedule next write event
     if(dumplogtime == 1) {
       if(logstep == 0) createevent(dumplog_dt0, dumpevent, NULL, 102) ;
-      else createevent(pow(dumplog_base, logstep-1) * dumplog_dt0, dumpevent, NULL, 102) ;
+      else createevent(pow(dumplog_base, logstep-1) * (dumplog_base-1.0) * dumplog_dt0, dumpevent, NULL, 102) ;
       if( logstep == dumplog_cyclelength ) logstep = 0 ;
       else logstep ++ ;
     } else createevent(snapshotinterval, dumpevent, NULL, 102) ;
